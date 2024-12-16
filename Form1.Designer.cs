6749cb9540c0f7e8b726a10a669fbbfc0bc045f0
Ksen1Bog <ksyshkagood@gmail.com>
2024-12-17 02:07:56 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Redactor2
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnLoadImage = new System.Windows.Forms.Button();
+            this.btnDetectTextRegions = new System.Windows.Forms.Button();
+            this.btnExtractText = new System.Windows.Forms.Button();
+            this.btnStartVideo = new System.Windows.Forms.Button();
+            this.btnStopVideo = new System.Windows.Forms.Button();
+            this.btnSaveImage = new System.Windows.Forms.Button();
+            this.btnApplyMask = new System.Windows.Forms.Button();
+            this.pictureBoxOriginal = new System.Windows.Forms.PictureBox();
+            this.pictureBoxProcessed = new System.Windows.Forms.PictureBox();
+
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProcessed)).BeginInit();
+            this.SuspendLayout();
+        
+            this.btnLoadImage.Location = new System.Drawing.Point(12, 12);
+            this.btnLoadImage.Name = "btnLoadImage";
+            this.btnLoadImage.Size = new System.Drawing.Size(150, 30);
+            this.btnLoadImage.Text = "Загрузить изображение";
+            this.btnLoadImage.UseVisualStyleBackColor = true;
+            this.btnLoadImage.Click += new System.EventHandler(this.btnLoadImage_Click);
+
+            this.btnDetectTextRegions.Location = new System.Drawing.Point(12, 48);
+            this.btnDetectTextRegions.Name = "btnDetectTextRegions";
+            this.btnDetectTextRegions.Size = new System.Drawing.Size(150, 30);
+            this.btnDetectTextRegions.Text = "Выделить текст";
+            this.btnDetectTextRegions.UseVisualStyleBackColor = true;
+            this.btnDetectTextRegions.Click += new System.EventHandler(this.btnDetectTextRegions_Click);
+
+      
+            this.btnExtractText.Location = new System.Drawing.Point(12, 84);
+            this.btnExtractText.Name = "btnExtractText";
+            this.btnExtractText.Size = new System.Drawing.Size(150, 30);
+            this.btnExtractText.Text = "Распознать текст";
+            this.btnExtractText.UseVisualStyleBackColor = true;
+            this.btnExtractText.Click += new System.EventHandler(this.btnExtractText_Click);
+
+            this.btnStartVideo.Location = new System.Drawing.Point(12, 120);
+            this.btnStartVideo.Name = "btnStartVideo";
+            this.btnStartVideo.Size = new System.Drawing.Size(150, 30);
+            this.btnStartVideo.Text = "Запустить видео";
+            this.btnStartVideo.UseVisualStyleBackColor = true;
+            this.btnStartVideo.Click += new System.EventHandler(this.btnStartVideo_Click);
+
+            this.btnStopVideo.Location = new System.Drawing.Point(12, 156);
+            this.btnStopVideo.Name = "btnStopVideo";
+            this.btnStopVideo.Size = new System.Drawing.Size(150, 30);
+            this.btnStopVideo.Text = "Остановить видео";
+            this.btnStopVideo.UseVisualStyleBackColor = true;
+            this.btnStopVideo.Click += new System.EventHandler(this.btnStopVideo_Click);
+
+            this.btnSaveImage.Location = new System.Drawing.Point(12, 192);
+            this.btnSaveImage.Name = "btnSaveImage";
+            this.btnSaveImage.Size = new System.Drawing.Size(150, 30);
+            this.btnSaveImage.Text = "Сохранить";
+            this.btnSaveImage.UseVisualStyleBackColor = true;
+            this.btnSaveImage.Click += new System.EventHandler(this.btnSaveImage_Click);
+
+            this.pictureBoxOriginal.Location = new System.Drawing.Point(200, 12);
+            this.pictureBoxOriginal.Name = "pictureBoxOriginal";
+            this.pictureBoxOriginal.Size = new System.Drawing.Size(400, 300);
+            this.pictureBoxOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+
+            this.pictureBoxProcessed.Location = new System.Drawing.Point(620, 12);
+            this.pictureBoxProcessed.Name = "pictureBoxProcessed";
+            this.pictureBoxProcessed.Size = new System.Drawing.Size(400, 300);
+            this.pictureBoxProcessed.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+
+            this.btnApplyMask = new System.Windows.Forms.Button();
+            this.btnApplyMask.Location = new System.Drawing.Point(12, 228);
+            this.btnApplyMask.Name = "btnApplyMask";
+            this.btnApplyMask.Size = new System.Drawing.Size(150, 30);
+            this.btnApplyMask.Text = "Наложить маску";
+            this.btnApplyMask.UseVisualStyleBackColor = true;
+            this.btnApplyMask.Click += new System.EventHandler(this.btnApplyMask_Click);
+
+            this.ClientSize = new System.Drawing.Size(1040, 330);
+            this.Controls.Add(this.btnLoadImage);
+            this.Controls.Add(this.btnDetectTextRegions);
+            this.Controls.Add(this.btnExtractText);
+            this.Controls.Add(this.btnStartVideo);
+            this.Controls.Add(this.btnStopVideo);
+            this.Controls.Add(this.btnSaveImage);
+            this.Controls.Add(this.pictureBoxOriginal);
+            this.Controls.Add(this.pictureBoxProcessed);
+            this.Controls.Add(this.btnApplyMask);
+            this.Text = "Обработка изображений";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button btnLoadImage;
+        private System.Windows.Forms.Button btnDetectTextRegions;
+        private System.Windows.Forms.Button btnExtractText;
+        private System.Windows.Forms.Button btnStartVideo;
+        private System.Windows.Forms.Button btnStopVideo;
+        private System.Windows.Forms.Button btnSaveImage;
+        private System.Windows.Forms.Button btnApplyMask;
+        private System.Windows.Forms.PictureBox pictureBoxOriginal;
+        private System.Windows.Forms.PictureBox pictureBoxProcessed;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.OCR;
+using Emgu.CV.Util;
+
+namespace Redactor2
+{
+    public partial class Form1 : Form
+    {
+        private Image<Bgr, byte> originalImage;  
+        private Image<Bgr, byte> processedImage; 
+        private VideoCapture videoCapture;
+        private Tesseract ocr;
+        private CascadeClassifier faceDetector;
+
+        public Form1()
+        {
+            InitializeComponent();
+            string tessdataPath = System.IO.Path.Combine(Application.StartupPath, "Resources", "tessdata");
+            string haarcascadePath = System.IO.Path.Combine(Application.StartupPath, "Resources", "frontalface", "haarcascade_frontalface_default.xml");
+            // Инициализация Tesseract и каскада Хаара
+            ocr = new Tesseract(tessdataPath, "eng", OcrEngineMode.Default);
+            faceDetector = new CascadeClassifier(haarcascadePath);
+        }
+
+        private void btnLoadImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp",
+                Title = "Выберите изображение"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                originalImage = new Image<Bgr, byte>(openFileDialog.FileName);
+                processedImage = originalImage.Clone();
+                pictureBoxOriginal.Image = originalImage.ToBitmap();
+                pictureBoxProcessed.Image = processedImage.ToBitmap();
+            }
+        }
+
+        private void btnDetectTextRegions_Click(object sender, EventArgs e)
+        {
+            if (originalImage != null)
+            {
+                var gray = originalImage.Convert<Gray, byte>();
+                var binary = new Mat();
+                CvInvoke.Threshold(gray, binary, 0, 255, ThresholdType.Otsu);
+
+                var contours = new VectorOfVectorOfPoint();
+                CvInvoke.FindContours(binary, contours, null, RetrType.External, ChainApproxMethod.ChainApproxSimple);
+
+                var resultImage = originalImage.Clone();
+
+                for (int i = 0; i < contours.Size; i++)
+                {
+                    var rect = CvInvoke.BoundingRectangle(contours[i]);
+                    if (rect.Width > 20 && rect.Height > 20) 
+                    {
+                        CvInvoke.Rectangle(resultImage, rect, new MCvScalar(0, 255, 0), 2); 
+                    }
+                }
+
+                processedImage = resultImage;
+                pictureBoxProcessed.Image = processedImage.ToBitmap();
+            }
+            else
+            {
+                MessageBox.Show("Сначала загрузите изображение!");
+            }
+        }
+
+        private void btnStartVideo_Click(object sender, EventArgs e)
+        {
+            if (videoCapture == null)
+            {
+                videoCapture = new VideoCapture();
+                Application.Idle += ProcessVideoFrame;
+            }
+        }
+
+        private void ProcessVideoFrame(object sender, EventArgs e)
+        {
+            var frame = new Mat();
+            videoCapture.Read(frame);
+
+            if (!frame.IsEmpty)
+            {
+                var grayFrame = new Mat();
+                CvInvoke.CvtColor(frame, grayFrame, ColorConversion.Bgr2Gray);
+
+                var faces = faceDetector.DetectMultiScale(grayFrame, 1.1, 4, Size.Empty, Size.Empty);
+
+                foreach (var face in faces)
+                {
+                    CvInvoke.Rectangle(frame, face, new MCvScalar(255, 0, 0), 2);
+                }
+
+                pictureBoxOriginal.Image = frame.ToBitmap();
+            }
+        }
+
+        private void btnStopVideo_Click(object sender, EventArgs e)
+        {
+            if (videoCapture != null)
+            {
+                var frame = new Mat();
+                videoCapture.Read(frame);
+
+                if (!frame.IsEmpty)
+                {
+                    processedImage = frame.ToImage<Bgr, byte>(); 
+                    pictureBoxProcessed.Image = processedImage.ToBitmap();
+                }
+
+                Application.Idle -= ProcessVideoFrame;
+                videoCapture.Dispose();
+                videoCapture = null;
+            }
+        }
+        private void btnExtractText_Click(object sender, EventArgs e)
+        {
+            if (processedImage != null)
+            {
+                var grayImage = processedImage.Convert<Gray, byte>();
+                ocr.SetImage(grayImage);
+                ocr.Recognize();
+                string text = ocr.GetUTF8Text();
+                MessageBox.Show($"Распознанный текст:\n{text}");
+            }
+            else
+            {
+                MessageBox.Show("Сначала обработайте изображение!");
+            }
+        }
+
+        private void btnApplyMask_Click(object sender, EventArgs e)
+        {
+            if (processedImage != null)
+            {
+                var grayImage = processedImage.Convert<Gray, byte>();
+                var faces = faceDetector.DetectMultiScale(grayImage, 1.1, 4, Size.Empty, Size.Empty);
+
+                foreach (var face in faces)
+                {
+                    CvInvoke.Rectangle(processedImage, face, new MCvScalar(0, 0, 255), -1); 
+                }
+
+                pictureBoxProcessed.Image = processedImage.ToBitmap();
+            }
+            else
+            {
+                MessageBox.Show("Сначала обработайте изображение!");
+            }
+        }
+
+        private void btnSaveImage_Click(object sender, EventArgs e)
+        {
+            if (processedImage != null)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog
+                {
+                    Filter = "PNG Image|*.png",
+                    Title = "Сохранить обработанное изображение"
+                };
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    processedImage.Save(saveFileDialog.FileName);
+                    MessageBox.Show("Изображение успешно сохранено!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Сначала обработайте изображение!");
+            }
+        }
+    }
+}
